e788c519945ea6adc5759f01c7332a2d41d71be9
Ahmet Burak Karhan <aburakkarhan@gmail.com>
2024-08-14 12:39:22 +0000

    update: modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
+import Navbar from "./components/Navbar/Navbar";
 import "./layout.scss";
 
 const App = () => {
-  return <div className="layout">Hello world!!!</div>;
+  return (
+    <div className="layout">
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
