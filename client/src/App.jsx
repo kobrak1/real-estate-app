1bd7b8f7865c5fcd26e2bb60443527798c989b75
Ahmet Burak Karhan <aburakkarhan@gmail.com>
2024-08-18 13:03:57 +0000

    modified:
    App.jsx and layout.scss has been modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./pages/HomePage/HomePage";
 import "./layout.scss";
 
 const App = () => {
   return (
     <div className="layout">
-      <Navbar />
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <HomePage />
+      </div>
     </div>
   );
 };
