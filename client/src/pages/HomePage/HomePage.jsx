3fd71d8461a2f2f54205a1bb8ca70ba83f0c34dc
kobrak1 <burakkarhan657@gmail.com>
2024-08-22 15:49:44 +0300

    add: HomePage component has been added

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,31 +1,28 @@
-import SearchBar from "../../components/SearchBar/SearchBar";
 import InfoBox from "../../components/InfoBox/InfoBox";
-import "./HomePage.scss";
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
 
-const HomePage = () => {
+function HomePage() {
   return (
     <div className="homePage">
       <div className="textContainer">
         <div className="wrapper">
           <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
-          <p>
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Interdum nisi
-            tempus eget pulvinar quisque tempor. Nulla class a et nibh mauris
-            risus. Lorem ipsum odor amet, consectetuer adipiscing elit.
+          <p className="paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+            explicabo suscipit cum eius, iure est nulla animi consequatur
+            facilis id pariatur fugit quos laudantium temporibus dolor ea
+            repellat provident impedit!
           </p>
           <SearchBar />
-          <div className="boxes">
-            <InfoBox stat="20+" description="Years of experience" />
-            <InfoBox stat="154" description="Award gained" />
-            <InfoBox stat="3000+" description="Property ready" />
-          </div>
+          <InfoBox />
         </div>
       </div>
       <div className="imgContainer">
-        <img src="../../../public/bg.png" />
+        <img src="/bg.png" alt="" />
       </div>
     </div>
   );
-};
+}
 
 export default HomePage;
