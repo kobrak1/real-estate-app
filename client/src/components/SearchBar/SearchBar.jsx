bbef3324604ab1f174c333930a74df77ac7e1035
kobrak1 <burakkarhan657@gmail.com>
2024-08-22 15:50:15 +0300

    add: SearchBar component has been added

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,67 @@
-import "./SearchBar.scss";
+import { useState } from "react"
+import "./SearchBar.scss"
 
 const SearchBar = () => {
-  return <div className="searchBar">Search Bar</div>;
-};
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  })
 
-export default SearchBar;
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setQuery({...query, [name]: value})
+  }
+
+  return (
+    <div className="searchBar">
+      <div className="type">
+        <button 
+          onClick={() => setQuery({...query, type: "buy"})}
+          className={query.type === "buy" ? "active" : ""}
+        >
+          Buy
+        </button>
+        <button 
+          onClick={() => setQuery({...query, type: "rent"})}
+          className={query.type === "rent" ? "active" : ""}
+        >
+          Rent
+        </button>
+      </div>
+      <form>
+        <input 
+          type="text" 
+          name="location"
+          value={query.location}
+          onChange={handleChange}
+          placeholder="City location"
+        />
+        <input 
+          type="number" 
+          name="minPrice" 
+          value={query.minPrice}
+          onChange={handleChange}
+          min={0} 
+          max={100000} 
+          placeholder="Min Price" 
+        />
+        <input 
+          type="number" 
+          name="maxPrice"
+          value={query.maxPrice}
+          onChange={handleChange}
+          min={0} 
+          max={100000} 
+          placeholder="Max Price" 
+        />
+        <button type="submit">
+          <img src="../../../public/search.png" alt="search_icon_not_found" />
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default SearchBar
