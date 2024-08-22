cdb85b0ca6eb05014460c05c910fdb2cd6f8dd69
kobrak1 <burakkarhan657@gmail.com>
2024-08-22 15:48:43 +0300

    modified: modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,35 @@
 import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./pages/HomePage/HomePage";
+import ListPage from "./pages/ListPage/ListPage";
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import "./layout.scss";
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+    {
+      path: "/list",
+      element: <ListPage />
+    }
+  ])
+
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <HomePage />
-      </div>
-    </div>
-  );
-};
+    // <div className="layout">
+    //   <div className="navbar">
+    //     <Navbar />
+    //   </div>
+    //   <div className="content">
+    //     <HomePage />
+    //   </div>
+    // </div>
+    <>
+      <Navbar />
+      <RouterProvider router={router} />
+    </>
+  )
+}
 
 export default App;
