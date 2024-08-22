5078ca673e9d732c994a9877b0ebbce6e695a55a
kobrak1 <burakkarhan657@gmail.com>
2024-08-22 15:49:16 +0300

    add: InfoBox component has been added

--- client/src/components/InfoBox/InfoBox.jsx
+++ client/src/components/InfoBox/InfoBox.jsx
@@ -1,10 +1,20 @@
 import "./InfoBox.scss";
 
-const InfoBox = ({ stat, description }) => {
+const InfoBox = () => {
   return (
-    <div className="info-box">
-      <h1>{stat}</h1>
-      <h2>{description}</h2>
+    <div className="boxes">
+      <div className="box">
+        <h1>16+</h1>
+        <h2>Years of Experience</h2>
+      </div>
+      <div className="box">
+        <h1>200</h1>
+        <h2>Award Gained</h2>
+      </div>
+      <div className="box">
+        <h1>2000+</h1>
+        <h2>Property Ready</h2>
+      </div>
     </div>
   );
 };
