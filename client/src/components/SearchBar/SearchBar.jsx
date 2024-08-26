1227eec9a473c5fd93e16de66271995648c370b2
Ahmet Burak Karhan <aburakkarhan@gmail.com>
2024-08-26 10:24:23 +0000

    modified:
    handleSubmit functionality has been integrated to the SearchBar component

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react"
-import "./SearchBar.scss"
+import { useState } from "react";
+import "./SearchBar.scss";
 
 const SearchBar = () => {
   const [query, setQuery] = useState({
@@ -7,61 +7,70 @@ const SearchBar = () => {
     location: "",
     minPrice: 0,
     maxPrice: 0,
-  })
+  });
 
   const handleChange = (e) => {
-    const {name, value} = e.target
-    setQuery({...query, [name]: value})
-  }
+    const { name, value } = e.target;
+    setQuery({ ...query, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      console.log("Form Submitted Successfully");
+    } catch (error) {
+      console.error("Form Submission Failed:", error);
+    }
+  };
 
   return (
     <div className="searchBar">
       <div className="type">
-        <button 
-          onClick={() => setQuery({...query, type: "buy"})}
+        <button
+          onClick={() => setQuery({ ...query, type: "buy" })}
           className={query.type === "buy" ? "active" : ""}
         >
           Buy
         </button>
-        <button 
-          onClick={() => setQuery({...query, type: "rent"})}
+        <button
+          onClick={() => setQuery({ ...query, type: "rent" })}
           className={query.type === "rent" ? "active" : ""}
         >
           Rent
         </button>
       </div>
-      <form>
-        <input 
-          type="text" 
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
           name="location"
           value={query.location}
           onChange={handleChange}
           placeholder="City location"
         />
-        <input 
-          type="number" 
-          name="minPrice" 
+        <input
+          type="number"
+          name="minPrice"
           value={query.minPrice}
           onChange={handleChange}
-          min={0} 
-          max={100000} 
-          placeholder="Min Price" 
+          min={0}
+          max={100000}
+          placeholder="Min Price"
         />
-        <input 
-          type="number" 
+        <input
+          type="number"
           name="maxPrice"
           value={query.maxPrice}
           onChange={handleChange}
-          min={0} 
-          max={100000} 
-          placeholder="Max Price" 
+          min={0}
+          max={100000}
+          placeholder="Max Price"
         />
         <button type="submit">
           <img src="../../../public/search.png" alt="search_icon_not_found" />
         </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
