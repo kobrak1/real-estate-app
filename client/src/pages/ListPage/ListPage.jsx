24689fe992ed28abca41f92408b23a290dd4e700
Ahmet Burak Karhan <aburakkarhan@gmail.com>
2024-08-28 11:29:26 +0000

    modified:
    modified

--- client/src/pages/ListPage/ListPage.jsx
+++ client/src/pages/ListPage/ListPage.jsx
@@ -1,3 +1,5 @@
+import Card from "../../components/Card/Card";
+import Filter from "../../components/Filter/Filter";
 import { listData } from "../../lib/dummyData";
 import "./ListPage.scss";
 
@@ -6,7 +8,14 @@ const ListPage = () => {
 
   return (
     <div className="listPage">
-      <div className="listContainer">Burak</div>
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {data.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
       <div className="mapContainer">Turkey</div>
     </div>
   );
