eb5f53913a279f697c68c33b0491809ff2c3250c
Ahmet Burak Karhan <aburakkarhan@gmail.com>
2024-08-31 18:18:13 +0000

    chore:
    chore

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import PromisePolyfill from "promise-polyfill";
 import App from "./App.jsx";
 import "./index.scss";
 
+if (!window.Promise) {
+    window.Promise = PromisePolyfill;
+}
+
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-      <App />
-  </StrictMode>
+    <StrictMode>
+        <App />
+    </StrictMode>,
 );
