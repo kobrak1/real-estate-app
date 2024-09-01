6f1357c3ed881cbf4f7713d1517c4f63e897aa8c
kobrak1 <burakkarhan657@gmail.com>
2024-09-01 22:07:58 +0300

    modified: SinglePage.jsx and Slider.jsx have been modified

---
+++ client/src/components/Slider/Slider.jsx
@@ -0,0 +1,11 @@
+import "./Slider.scss"
+
+const Slider = ({images}) => {
+    return (
+        <div className="slider">
+            Slider
+        </div>
+    )
+}
+
+export default Slider
--- client/src/pages/SinglePage/SinglePage.jsx
+++ client/src/pages/SinglePage/SinglePage.jsx
@@ -1,7 +1,39 @@
+import Slider from "../../components/Slider/Slider.jsx"
 import "./SinglePage.scss";
+import { singlePostData, userData } from "../../lib/dummyData.js"
 
 const SinglePage = () => {
-  return <div>Hello Single Item</div>;
+  return (
+    <div className="single-page">
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={singlePostData.images}/>
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="address">
+                  <img src="/pin.png" alt="pin.png not found"/>
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className="price">
+                  {singlePostData.price}
+                </div>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="user image not found" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            <div className="bottom"></div>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper"></div>
+      </div>
+    </div>
+  )
 };
 
 export default SinglePage;
