6be2f4b05011175388c407332bfae864a39a6e86
kobrak1 <burakkarhan657@gmail.com>
2024-09-13 21:24:36 +0300

    add: ProfilePage component and its style file has been added

---
+++ client/src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import "./ProfilePage.scss"
+
+const ProfilePage = () => {
+    return (
+        <div>
+            Burak Karhan Profile Page
+        </div>
+    )
+}
+
+export default ProfilePage
