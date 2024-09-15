1ed5a54546b03108d58f80f458577df4de622f20
kobrak1 <burakkarhan657@gmail.com>
2024-09-15 13:28:04 +0300

    modified: InfoBox code reformatted

--- client/src/components/InfoBox/InfoBox.jsx
+++ client/src/components/InfoBox/InfoBox.jsx
@@ -1,19 +1,21 @@
 import "./InfoBox.scss";
 
 const InfoBox = () => {
+  const boxes = [
+    { number: "16+", text: "Years of Experience" },
+    { number: "7", text: "Awards Gained" },
+    { number: "3000+", text: "Property Ready" },
+  ];
+
   return (
-    <div className="boxes">
-      <div className="box">
-        <h1>16+</h1>
-        <h2>Years of Experience</h2>
-      </div>
-      <div className="box">
-        <h1>200</h1>
-        <h2>Award Gained</h2>
-      </div>
-      <div className="box">
-        <h1>2000+</h1>
-        <h2>Property Ready</h2>
+    <div className="boxes-container">
+      <div className="boxes">
+        {[...boxes, ...boxes].map((box, index) => (
+          <div className="box" key={index}>
+            <h1>{box.number}</h1>
+            <h2>{box.text}</h2>
+          </div>
+        ))}
       </div>
     </div>
   );
