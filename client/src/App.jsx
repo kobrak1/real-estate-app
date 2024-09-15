d7ff832e662471562a947a6dd8969efaaf1c8768
kobrak1 <burakkarhan657@gmail.com>
2024-09-15 13:21:53 +0300

    update: ProfilePage route has been added to the router object in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import Layout from "./pages/layout/layout.jsx";
 import HomePage from "./pages/HomePage/HomePage.jsx";
 import SinglePage from "./pages/SinglePage/SinglePage.jsx";
+import ProfilePage from "./pages/ProfilePage/ProfilePage.jsx";
 import ListPage from "./pages/ListPage/ListPage.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -22,6 +23,10 @@ const App = () => {
           path: "/:id",
           element: <SinglePage />,
         },
+        {
+          path: "/profile",
+          element: <ProfilePage />
+        }
       ],
     },
   ]);
