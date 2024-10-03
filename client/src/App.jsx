01b73999d13376c02777026aa94d49899b12b276
kobrak1 <burakkarhan657@gmail.com>
2024-10-03 23:59:43 +0300

    modified: LoginPage component has been imported

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Mosaic } from "react-loading-indicators";
 import Layout from "./pages/layout/layout.jsx";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary.jsx";
-import LoginPage from "./pages/LoginPage/LoginPage.jsx";
 
 // Lazy load components
 const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
 const SinglePage = lazy(() => import("./pages/SinglePage/SinglePage.jsx"));
 const ProfilePage = lazy(() => import("./pages/ProfilePage/ProfilePage.jsx"));
 const ListPage = lazy(() => import("./pages/ListPage/ListPage.jsx"));
+const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage.jsx"));
 
 const App = () => {
   const router = createBrowserRouter([
