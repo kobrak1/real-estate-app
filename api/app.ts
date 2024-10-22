48e5c6e29e0b06cbc27ed118f1f56fb61cb8143f
kobrak1 <burakkarhan657@gmail.com>
2024-10-22 23:42:23 +0300

    modified: app.js modified to app.ts

---
+++ api/app.ts
@@ -0,0 +1,21 @@
+import express, { Express } from "express"
+import cors from "cors";
+import { unknownEndpoint, errorHandler } from "./middlewares/errorHandler"
+import { reqLogger } from "./middlewares/requestLogger"
+
+// Routers
+import authRouterV1 from "./routers/v1/auth.route"
+
+const app: Express = express()
+
+// Middlewares
+app.use(cors())
+app.use(express.json())
+app.use(reqLogger)
+
+app.use("/api/auth/v1", authRouterV1)
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+export default app;
