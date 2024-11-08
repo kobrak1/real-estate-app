ddbe30f7d1500ca675f2a5aed1cfcc536e3c7774
kobrak1 <aburakkarhan@gmail.com>
2024-11-08 23:30:40 +0300

    modified: added the configuration logic for allowedOrigin variable

--- api/utils/config.ts
+++ api/utils/config.ts
@@ -1,8 +1,18 @@
 import { config } from "dotenv"
 config()
 
+// Allowed origins for both dev and prod environments
+const allowedOrigin: string = process.env.NODE_ENV === "production"
+    ? "https://estate-listing.onrender.com"
+    : "http://localhost:3000"
+
+// Port number server runs on
 const port: string | undefined = process.env.PORT
+
+// Secret key for JWT token
 const secret: string | undefined = process.env.SECRET
+
+// Connection string for mongodb connection
 const mongodb_uri: string | undefined = 
     process.env.NODE_ENV === "test"
         ? process.env.TEST_MONGODB_URI
@@ -13,4 +23,9 @@ if (!port || !secret || !mongodb_uri) {
     throw new Error("Mising required environment variables")
 }
 
-export { port, secret, mongodb_uri }
+export { 
+    port, 
+    secret, 
+    mongodb_uri,
+    allowedOrigin,
+}
