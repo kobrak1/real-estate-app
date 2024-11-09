49a25919de56c305f99e5308b8ff465cac4a9907
kobrak1 <aburakkarhan@gmail.com>
2024-11-09 15:41:06 +0300

    chore: added for debugging

--- client/src/services/serviceConfig.js
+++ client/src/services/serviceConfig.js
@@ -4,6 +4,8 @@ const apiUrl = import.meta.env.PROD
     ? import.meta.env.VITE_PROD_API_URL  // url of the deployed service
     : import.meta.env.VITE_DEV_API_URL   //  url of the develeopment server
 
+console.log("apiURL:", apiUrl)
+
 export const api = axios.create({
     baseURL: apiUrl,
     withCredentials: true
