13b9f9ce3548158564e18aadf0c6979674b80707
kobrak1 <aburakkarhan@gmail.com>
2024-11-14 14:01:51 +0300

    modified: bg image has been added to the authLayout

--- client/src/components/AuthFallbackUI/AuthFallbackUI.jsx
+++ client/src/components/AuthFallbackUI/AuthFallbackUI.jsx
@@ -5,9 +5,11 @@ import './AuthFallbackUI.scss';
 const AuthFallbackUI = () => {
   return (
     <div className="page-not-found">
-      <h1>404</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
-      <Link to="/auth/login" className="auth-login-link">Go back to login page</Link>
+      <div className="content">
+        <h1>404</h1>
+        <p>Sorry, the page you are looking for does not exist.</p>
+        <Link to="/auth/login" className="auth-login-link">Go back to login page</Link>
+      </div>
     </div>
   );
 };
